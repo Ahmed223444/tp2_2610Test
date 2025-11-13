1002e50735022ceac3c12ef8d54ffb47ed229f03
Ahmed223444 <144179889+Ahmed223444@users.noreply.github.com>
2025-11-13 08:38:07 -0500

    Modify server.h to enhance video management
    Refactor ServerMonitor structure and update function signatures.

--- server.h
+++ server.h
@@ -2,41 +2,25 @@
 #define SERVER_H
 
 #include <pthread.h>
-#include <stdbool.h>
-
-#define MAX_VIDEOS 10
-#define CONTENT_SIZE 256
 
 typedef struct {
     int id;
-    int duration;
-    char content[CONTENT_SIZE];
+    char titre[128];
+    int duree;
 } Video;
 
 typedef struct {
-    Video buffer[MAX_VIDEOS];
-    int in;
-    int out;
-    int count;
-    bool active;
-    
-    // Nouveau: suivi des vidéos en cours de traitement
-    int beingProcessed[MAX_VIDEOS]; // 0 = libre, 1 = en cours de diffusion
-    int processingStarted[MAX_VIDEOS]; // timestamp du début de traitement
-    
+    Video videos[100];
+    int nb_videos;
+    int panne;
     pthread_mutex_t mutex;
-    pthread_cond_t notFull;
-    pthread_cond_t notEmpty;
+    pthread_cond_t cond;
 } ServerMonitor;
 
-void initServer(ServerMonitor* server);
-void destroyServer(ServerMonitor* server);
-
-void sendVideo(ServerMonitor* server, Video video);
-Video getVideo(ServerMonitor* server);
-void completeVideo(ServerMonitor* server, int index);
-
-// Nouvelle fonction pour récupérer les vidéos en cours de traitement
-int getProcessingVideos(ServerMonitor* server, Video* output);
+void init_server(ServerMonitor* serveur);
+void* server_thread(void* arg);
+void ajouter_video(ServerMonitor* serveur, const Video* v);
+void simuler_panne(ServerMonitor* serveur);
+void restaurer_serveur_principal(ServerMonitor* serveur);
 
 #endif
