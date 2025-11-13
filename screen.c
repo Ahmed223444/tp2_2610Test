876e656b0a5e75d1ebf11d9551673936588463f8
Ahmed223444 <144179889+Ahmed223444@users.noreply.github.com>
2025-11-13 08:35:53 -0500

    Refactor video broadcasting logic in screen.c
    Removed validation check for received video and added sleep after broadcasting.

--- screen.c
+++ screen.c
@@ -1,13 +1,14 @@
+
 #include <stdio.h>
 #include <unistd.h>
 #include "screen.h"
 #include "server.h"
 
 void broadcastVideo(Video video) {
+    
     printf("[SCREEN] Diffusion de la vidéo %d : %s (durée %d s)\n",
            video.id, video.content, video.duration);
 
-    // Simulation de la diffusion
     sleep(video.duration);
 }
 
@@ -16,18 +17,13 @@ void* screenThread(void* arg) {
 
     while (1) {
         Video video = getVideo(server);
-        
-        // Vérifier si on a reçu une vidéo valide (le serveur pourrait être inactif)
-        if (video.id == 0) {
-            usleep(100000); // Attendre 100ms avant de réessayer
-            continue;
-        }
-        
         int index = server->out;
 
         broadcastVideo(video);
 
         completeVideo(server, index);
+
+        sleep(video.duration);
     }
 
     return NULL;
