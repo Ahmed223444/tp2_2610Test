dab8e3e7428b5116193edefc0139785e27a0aff1
Ahmed223444 <144179889+Ahmed223444@users.noreply.github.com>
2025-11-13 08:44:04 -0500

    Update systeme.c

--- systeme.c
+++ systeme.c
@@ -1,34 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <unistd.h>
+#include <time.h>
+#include <signal.h>
 #include "server.h"
+#include "camera.h"
+#include "screen.h"
 #include "backup_server.h"
 
+ServerMonitor mainServer;
+BackupServer backupServer;
+pthread_t cameraThreadId;
+pthread_t screenThreadId;
+
+int systemCrashed = 0;
+
+void simulateCrash() {
+    if (systemCrashed) {
+        printf("[SYSTEME] Le système est déjà en panne.\n");
+        return;
+    }
+
+    printf("\n[!! PANNE] Panne simulée ! Sauvegarde en cours...\n");
+    backupVideos(&backupServer, &mainServer);
+    printf("[!! PANNE] Sauvegarde terminée.\n");
+
+    pthread_cancel(cameraThreadId);
+    pthread_cancel(screenThreadId);
+    systemCrashed = 1;
+
+    printf("[!! PANNE] Tous les processus ont été interrompus.\n");
+    printf("[SYSTEME] Tapez 'r' pour restaurer le serveur, ou 'q' pour quitter.\n");
+}
+
+void handleCrashSignal(int sig) {
+    printf("\n[SIGNAL] Signal reçu (%d), simulation de panne...\n", sig);
+    simulateCrash();
+}
+
+void restoreSystem() {
+    if (!systemCrashed) {
+        printf("[SYSTEME] Aucun besoin de restauration.\n");
+        return;
+    }
+
+    printf("\n[RESTORE] Restauration du serveur principal à partir de la sauvegarde...\n");
+    restoreVideos(&backupServer, &mainServer);
+
+    pthread_create(&cameraThreadId, NULL, cameraThread, &mainServer);
+    pthread_create(&screenThreadId, NULL, screenThread, &mainServer);
+
+    systemCrashed = 0;
+    printf("[RESTORE] Restauration complète. Le système reprend son fonctionnement normal.\n");
+}
+
+void shutdownSystem() {
+    printf("\n[SYSTEME] Arrêt du système...\n");
+
+    pthread_cancel(cameraThreadId);
+    pthread_cancel(screenThreadId);
+    pthread_join(cameraThreadId, NULL);
+    pthread_join(screenThreadId, NULL);
+
+    destroyServer(&mainServer);
+    destroyBackupServer(&backupServer);
+
+    printf("[SYSTEME] Fin du programme.\n");
+}
+
 int main() {
-    ServerMonitor serveur;
-    pthread_t tid_server, tid_backup;
+    srand(time(NULL));
+    signal(SIGUSR1, handleCrashSignal);
 
-    init_server(&serveur);
+    initServer(&mainServer);
+    initBackupServer(&backupServer);
 
-    pthread_create(&tid_server, NULL, server_thread, &serveur);
-    pthread_create(&tid_backup, NULL, backup_server_thread, &serveur);
+    pthread_create(&cameraThreadId, NULL, cameraThread, &mainServer);
+    pthread_create(&screenThreadId, NULL, screenThread, &mainServer);
 
+    printf("[SYSTEME] Système opérationnel.\n");
+    printf("=== COMMANDES ===\n");
     printf("  p : simuler une panne\n");
     printf("  r : restaurer le serveur après panne\n");
     printf("  q : quitter proprement\n");
-    printf("==============================\n");
+    printf("=================\n");
 
-    char cmd;
+    char command;
     while (1) {
-        printf("> ");
-        scanf(" %c", &cmd);
-        if (cmd == 'p') simuler_panne(&serveur);
-        else if (cmd == 'r') restaurer_serveur_principal(&serveur);
-        else if (cmd == 'q') break;
-        else printf("[SYSTEME] Commande inconnue.\n");
-    }
+        printf("\n> ");
+        command = getchar();
+        if (command == '\n') continue;
 
-    printf("[SYSTEME] Arrêt propre.\n");
-    return 0;
+        switch (command) {
+            case 'p': raise(SIGUSR1); break;
+            case 'r': restoreSystem(); break;
+            case 'q': shutdownSystem(); return 0;
+            default: printf("[SYSTEME] Commande inconnue.\n");
+        }
+
+        while (getchar() != '\n');
+    }
 }
