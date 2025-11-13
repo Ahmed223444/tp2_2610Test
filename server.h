e435d1e2995b466816a28aaa0a9b779b0df3a868
Ahmed223444 <144179889+Ahmed223444@users.noreply.github.com>
2025-11-13 09:03:35 -0500

    Update server.h

--- server.h
+++ server.h
@@ -2,33 +2,36 @@
 #define SERVER_H
 
 #include <pthread.h>
+#include <stdbool.h>
 
-#define MAX_VIDEOS 100
-#define CONTENT_SIZE 256
+#define MAX_VIDEOS 10        // nombre maximum de vidéos dans le buffer
+#define CONTENT_SIZE 256     // taille du contenu d'une vidéo
 
 typedef struct {
     int id;
-    char content[CONTENT_SIZE];
     int duration;
+    char content[CONTENT_SIZE];
 } Video;
 
 typedef struct {
-    Video videos[MAX_VIDEOS];
-    int in;
-    int out;
-    int count;
+    Video buffer[MAX_VIDEOS];
+    int in;                  // index d'insertion
+    int out;                 // index de retrait
+    int count;               // nombre d'éléments présents
+    bool active;             // indique si le serveur est actif ou en panne
+    int beingProcessed[MAX_VIDEOS]; // 0 = libre, 1 = en cours de diffusion
+
     pthread_mutex_t mutex;
-    pthread_cond_t notEmpty;
     pthread_cond_t notFull;
+    pthread_cond_t notEmpty;
 } ServerMonitor;
 
-// Initialisation
 void initServer(ServerMonitor* server);
 void destroyServer(ServerMonitor* server);
 
-// Gestion du flux
 void sendVideo(ServerMonitor* server, Video video);
 Video getVideo(ServerMonitor* server);
+
 void completeVideo(ServerMonitor* server, int index);
 
 #endif
